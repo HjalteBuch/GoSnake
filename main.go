81843f0aba659ec007268a2bb976b347e9b4d5b3
Hjalte Hesbjerg <hjalte@Hjaltes-MacBook-Pro.local>
2024-01-28 14:49:43 +0900

    Added console logs. Added KeyboardEvent to switch statment and checking if specific key is pressed. (must be more efficient ways to do this instead of string comparrison)

--- main.go
+++ main.go
@@ -1,44 +1,62 @@
-package main 
+package main
 
 import (
-    "fmt"
-    "github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/sdl"
 )
 
+const w = 640
+const h = 480
+
 func main() {
-    fmt.Println("Starting snake")
+    println("Initializing SDL")
     if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
         panic(err)
     }
     defer sdl.Quit()
 
-    window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		800, 600, sdl.WINDOW_SHOWN)
-	if err != nil {
-		panic(err)
-	}
-	defer window.Destroy()
+    println("Initializing window")
+    window, err := sdl.CreateWindow("SDL Window", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, w, h, sdl.WINDOW_SHOWN)
+    if err != nil {
+        panic(err)
+    }
+    defer window.Destroy()
 
+    println("Getting surface")
     surface, err := window.GetSurface()
     if err != nil {
         panic(err)
     }
-    surface.FillRect(nil, 0)
+    println("Creating background on surface")
+    surface.FillRect(nil, 250)
 
-    rect := sdl.Rect{0, 0, 200, 200}
+    println("Creating object")
+    rect := sdl.Rect{w/2-20, h/2-20, 40, 40}
     colour := sdl.Color{R: 255, G: 0, B: 255, A: 255} // purple
     pixel := sdl.MapRGBA(surface.Format, colour.R, colour.G, colour.B, colour.A)
+    println("Adding object to surface")
     surface.FillRect(&rect, pixel)
+	
+
+    println("Initial updating of surface")
     window.UpdateSurface()
 
     running := true
     for running {
         for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-            switch event.(type) {
+            switch et := event.(type) {
             case *sdl.QuitEvent:
                 println("Quit")
                 running = false
                 break
+            case *sdl.KeyboardEvent:
+                if sdl.GetKeyName(et.Keysym.Sym) == "Left" {
+                    rect.X += -20
+                } else {
+                    rect.X += 20
+                }
+                surface.FillRect(nil, 250)
+                surface.FillRect(&rect, pixel)
+                window.UpdateSurface()
             }
         }
     }
