25e0f3f3d5d7e812cb573072fa1d8a30b5e5b4f4
Hjalte Hesbjerg <hjalte@Hjaltes-MacBook-Pro.local>
2024-01-31 11:48:35 +0900

    feat: Added snake object with functions to interact

--- main.go
+++ main.go
@@ -1,33 +1,51 @@
 package main
 
 import (
+	"time"
+
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+type Snake struct {
+    Body []sdl.Rect
+    Direction int
+}
+
+const (
+    RIGHT = iota
+    LEFT
+    UP
+    DOWN
+)
+const velocity = 20
 const w = 640
 const h = 480
 
 var window *sdl.Window
 var surface *sdl.Surface
-
+var snake Snake
+var pixel uint32
 
 func main() {
-    if sdlInit() {
+    if !sdlInit() {
         println("Something went wrong when initalizing sdl")
         closeSdl()
         return
     }
 
     println("Creating background on surface")
-    surface.FillRect(nil, 250)
+    surface.FillRect(nil, 0)
+
 
     println("Creating object")
-    rect := sdl.Rect{w/2-20, h/2-20, 40, 40}
-    colour := sdl.Color{R: 255, G: 0, B: 255, A: 255} // purple pixel := sdl.MapRGBA(surface.Format, colour.R, colour.G, colour.B, colour.A)
-    pixel := sdl.MapRGBA(surface.Format, colour.R, colour.G, colour.B, colour.A)
+    head := sdl.Rect{w/2-velocity/2, h/2-velocity/2, velocity, velocity}
+    colour := sdl.Color{R: 200, G: 200, B: 200, A: 200} // purple pixel := sdl.MapRGBA(surface.Format, colour.R, colour.G, colour.B, colour.A)
+    pixel = sdl.MapRGBA(surface.Format, colour.R, colour.G, colour.B, colour.A)
     println("Adding object to surface")
-    surface.FillRect(&rect, pixel)
+
+    snake = Snake{[]sdl.Rect{head}, 0}
 	
+    drawSnake()
 
     println("Initial updating of surface")
     window.UpdateSurface()
@@ -41,20 +59,49 @@ func main() {
                 running = false
                 break
             case *sdl.KeyboardEvent:
-                if et.Keysym.Sym == sdl.K_LEFT {
-                    rect.X += -20
-                } else if et.Keysym.Sym == sdl.K_RIGHT{
-                    rect.X += 20
-                }
-                
-                surface.FillRect(nil, 250)
-                surface.FillRect(&rect, pixel)
-                window.UpdateSurface()
+                changeSnakeDirection(et.Keysym.Sym)
             }
         }
+        moveSnake()
+        drawSnake()
+        window.UpdateSurface()
+        time.Sleep(time.Second/2)
     }
 
     closeSdl()
+}
+
+func moveSnake() {
+    switch snake.Direction {
+    case RIGHT:
+        snake.Body[0].X += velocity
+    case LEFT:
+        snake.Body[0].X -= velocity
+    case DOWN:
+        snake.Body[0].Y += velocity
+    case UP:
+        snake.Body[0].Y -= velocity
+    }
+}
+
+func changeSnakeDirection(key sdl.Keycode) {
+    switch key {
+    case sdl.K_LEFT:
+        snake.Direction = LEFT
+    case sdl.K_RIGHT:
+        snake.Direction = RIGHT
+    case sdl.K_UP:
+        snake.Direction = UP
+    case sdl.K_DOWN:
+        snake.Direction = DOWN
+    }
+}
+
+func drawSnake() {
+    surface.FillRect(nil, 0)
+    for i := 0; i < len(snake.Body); i++ {
+        surface.FillRect(&snake.Body[i], pixel)
+    }
 }
 
 func sdlInit() bool {
